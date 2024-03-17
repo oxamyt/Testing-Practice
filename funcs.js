89a991c38dd287a1d45e282898b83748bdfbd1f7
Oxamyt <dentez.danil@gmail.com>
2024-03-17 18:50:38 +0200

    create caesarCipher test and pass it with function

--- funcs.js
+++ funcs.js
@@ -20,3 +20,28 @@ export const calculator = {
     return a * b;
   },
 };
+
+export function caesarCipher(string) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const cipherString = [];
+  for (let i = 0; i < string.length; i++) {
+    let character = string.charAt(i);
+    if (!character.match(/[a-z]/i)) {
+      cipherString.push(character);
+    } else {
+      let characterAlphabet = alphabet.find(
+        (char) => char === character.toLowerCase()
+      );
+      if (alphabet.indexOf(characterAlphabet) + 3 >= alphabet.length) {
+        alphabet.push(..."abcdefghijklmnopqrstuvwxyz".split(""));
+      }
+      let cipherChar = alphabet[alphabet.indexOf(characterAlphabet) + 3];
+      if (character === character.toUpperCase()) {
+        cipherString.push(cipherChar.toUpperCase());
+      } else {
+        cipherString.push(cipherChar);
+      }
+    }
+  }
+  return cipherString.join("").toString();
+}
--- funcs.test.js
+++ funcs.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverse, calculator } from "./funcs";
+import { capitalize, reverse, calculator, caesarCipher } from "./funcs";
 
 test("return first character capitalized", () => {
   expect(capitalize("banana")).toBe("Banana");
@@ -13,4 +13,8 @@ test("calculator basic operations, add, subtract,divide,multiply", () => {
   expect(calculator.subtract(2, 10)).toBe(-8);
   expect(calculator.divide(2, 10)).toBeCloseTo(0.2);
   expect(calculator.multiply(2, 10)).toBe(20);
+});
+
+test("caesar cipher", () => {
+  expect(caesarCipher("*kjASD,ZZdz.j&&dsa")).toBe("*nmDVG,CCgc.m&&gvd");
 });
